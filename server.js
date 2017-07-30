7a9050abedd940ff89aa4e11687ae89d6ee9eaee
Kunal <1onlychess@gmail.com>
2017-07-30 23:53:13 +0530

    Add base repo files

--- server.js
+++ server.js
@@ -6,50 +6,40 @@ var Pool = require('pg').Pool;
 var crypto = require('crypto');
 var bodyParser = require('body-parser');
 var session = require('express-session');
-var usern;var config = {
-    user: 'postgres',
-    database: 'any',
-    host: 'localhost',
-    port: '5432',
-    password: 'pkra8949'
-};
-
 app.use(morgan('combined'));
 app.use(bodyParser.json());
 app.use(session({
     secret: 'someRandomSecretValue',
     cookie: { maxAge: 1000 * 60 * 60 * 24 * 30}
 }));
-o
+
 app.get('/booked', function (req, res) {
 
-   res.sendFile(path.join(__dirname, 'form', 'booked.html'));
+   res.sendFile(path.join(__dirname, 'web', 'booked.html'));
 });
 
 app.get('/logout', function (req, res) {
-
-   res.sendFile(path.join(__dirname, 'form', 'login.html'));
-    res.sendFile(path.join(__dirname, 'form', 'login.css'));
-
+  delete req.session.auth;
+   res.sendFile(path.join(__dirname, 'web', 'samplelogin.html'));
 });
 
 app.get('/', function (req, res) {
 
-   res.sendFile(path.join(__dirname, 'form', 'login.html'));
+   res.sendFile(path.join(__dirname, 'web', 'samplelogin.html'));
 });
 
 app.get('/book', function (req, res) {
 
-   res.sendFile(path.join(__dirname, 'form', 'index.html'));
+   res.sendFile(path.join(__dirname, 'web', 'index.html'));
 
 });
 
 app.get('/:fileName', function (req, res) {
-  res.sendFile(path.join(__dirname, 'form', req.params.fileName));
+  res.sendFile(path.join(__dirname, 'web', req.params.fileName));
 });
 
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
-  console.log(`IMAD course app listening on port ${port}!`);
+  console.log(`listening on ${port}!`);
 });
